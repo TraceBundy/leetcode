16f96984835af2b724b8df10f46cfb00e67557e6
TraceBundy <yangzhou221@gmail.com>
2015-05-26 08:51:32 +0800

    climbingstairs

---
+++ climbingstairs.cc
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> res(3);
+        res[0] = 1;
+        res[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            res[i%3] = res[(i-1)%3] + res[(i-2)%3]; 
+        }
+        return res[n%3];
+    }
+};
