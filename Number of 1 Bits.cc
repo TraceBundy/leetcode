2247a7f94add1a805870ce639d86c4a359094a80
TraceBundy <yangzhou221@gmail.com>
2015-05-30 15:21:18 +0800

    add number 1 bits

---
+++ Number of 1 Bits.cc
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int i = 0;
+        while (n) {
+            ++i;
+            n = (n-1) & n;
+        }
+        return i;
+    }
+};
