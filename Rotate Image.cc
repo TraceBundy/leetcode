30570d31b631ad0e6a623d21a0ec3e2c01cf6b65
TraceBundy <yangzhou221@gmail.com>
2015-06-05 05:59:21 +0800

    add

--- Rotate Image.cc
+++ Rotate Image.cc
@@ -1,14 +1,16 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+        if (matrix.size() == 0) return;
         int n = matrix.size();
-        if (n == 0) return;
         int m = matrix[0].size();
+
         for (int i = 0; i < n-1; ++i){
             for (int j = 0; j < n-i; ++j){
                 swap(matrix[i][j], matrix[n-j-1][n-i-1]);
             }
         }
+
         for (int i = 0; i < n / 2; ++i){
             for (int j = 0; j < m; ++j){
                 swap(matrix[i][j], matrix[n-i-1][j]);
