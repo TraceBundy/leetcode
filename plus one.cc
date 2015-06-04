f0eb90c8018f553c60a73a53f11b7399a3f0421b
tracebundy <yangzhou221@gmail.com>
2015-06-05 00:34:59 +0800

    add'

--- plus one.cc
+++ plus one.cc
@@ -1,15 +1,15 @@
 class Solution {
 public
-    vectorint plusOne(vectorint& digits) {
+    vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-        vectorint res(n,0);
+        vector<int> res(n,0);
         int a = 1;
         for (int i = n-1; i = 0; i--){
             int sum = a + digits[i];
             a = sum  10;
             res[i] = sum % 10;
         }
-        if (a  0){
+        if (a > 0){
             res.insert(res.begin(), a);
         }
         return res;
