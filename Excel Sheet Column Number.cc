6fb1bcd980c93ac54c5e67dc1d14e80e78c74ba1
TraceBundy <yangzhou221@gmail.com>
2015-06-11 19:03:32 +0800

    add

---
+++ Excel Sheet Column Number.cc
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int sum = 0;
+        int len = s.length();
+        for (int i = len-1; i >= 0; i--){
+            sum += (s[i]-'A'+1)*pow(26, len-i-1);
+        }
+        return sum;
+    }
+};
